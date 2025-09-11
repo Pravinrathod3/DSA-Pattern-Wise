2ef32bf568e101931f6a85588d01ac3f5864b90e
Pravin Rathod <pravinrathod4532@gmail.com>
2025-09-11 10:31:19 +0530

    array revision

---
+++ array/rotate_array.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        if(n < 2){
+           return;
+        }
+        
+        reverse(nums.begin(), nums.end());
+
+        reverse(nums.begin(), nums.begin()+k);
+        reverse(nums.begin()+k, nums.end());
+    }
+};
+
+int main(){
+    Solution sol;
+    vector<int> nums = {1,2,3,4,5,6,7};
+    int k = 3;
+    sol.rotate(nums, k);
+    for(int num : nums){
+        cout << num << " ";
+    }
+    return 0;
+}
