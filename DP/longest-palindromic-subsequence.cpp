b6f289af5d72f17543193d6546b6cc301be7a089
Pravin Rathod <pravinrathod4532@gmail.com>
2026-01-28 11:32:43 +0530

    Add solution for longest palindromic subsequence using dynamic programming

---
+++ DP/longest-palindromic-subsequence.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+
+
+    int longestPalindromeSubseq(string s) {
+        int n = s.size();
+
+        string t = s;
+        reverse(t.begin(), t.end());   // reverse the string s to get t
+
+        vector<vector<int>> dp(n+1, vector<int> (n+1, 0));
+
+        for(int i=1; i<=n; i++){         
+            for(int j=1; j <= n; j++){
+                if(s[i-1] == t[j-1]){              // characters match
+                    dp[i][j] = 1+dp[i-1][j-1];        // include this character in LCS
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);   // characters do not match exclude one character and take max
+                }
+
+            }
+        }
+
+
+
+        return dp[n][n];
+
+        
+    }
+};
