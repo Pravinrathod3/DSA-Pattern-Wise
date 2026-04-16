21147bd28f02f4409a1b54965b3db84833951e89
Pravin Rathod <pravinrathod4532@gmail.com>
2026-04-16 10:26:28 +0530

    dfs

---
+++ trees/children-sum-in-binary-tree.cpp
@@ -0,0 +1,44 @@
+/*
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+*/
+
+class Solution {
+  public:
+  
+  
+  
+    bool isSumProperty(Node *root) {
+        
+      if(root == NULL) return true;
+       
+      if(root->left == NULL && root->right == NULL) return true;
+      
+
+      int sum = 0;
+      if(root->left != NULL) sum += root->left->data;
+      
+      if(root->right != NULL) sum += root->right->data;
+      
+      if(sum != root->data){
+          return false;
+      }
+      
+      if(isSumProperty(root->left) == false) return false;
+      if(isSumProperty(root->right) == false) return false;
+      
+      return true;
+    }
+};
