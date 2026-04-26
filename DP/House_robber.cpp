d95243e53175fc6982944ed5e75ff79722d667fb
Pravin Rathod <pravinrathod4532@gmail.com>
2026-04-26 12:35:36 +0530

    revison

--- DP/House_robber.cpp
+++ DP/House_robber.cpp
@@ -42,3 +42,27 @@ int main(){
 
 // Time Complexity: O(n)
 // Space Complexity: O(n)
+
+//Tabulation
+
+int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, 0);
+
+        if(n == 1) return nums[0];
+
+        dp[0] = nums[0];                      
+        dp[1] = max(nums[0], nums[1]);
+
+        for(int i=2; i<n; i++){
+            int take = dp[i-2] + nums[i];
+            int nottake = dp[i-1];
+
+            dp[i] = max(take, nottake);
+        }
+
+
+        return dp[n-1];
+
+        
+    }
